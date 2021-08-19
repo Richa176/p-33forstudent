6ca1de1b6236c14f10a8c44e60db4a767a9fc9de
Richa176 <69035220+Richa176@users.noreply.github.com>
2021-08-19 17:46:00 +0530

    updated

--- sketch.js
+++ sketch.js
@@ -1,9 +1,38 @@
+const Engine= Matter.Engine;
+const World= Matter.World;
+const Bodies= Matter.Bodies;
+
+var engine,world;
+var bgImg;
+var snow=[];
+var snow1,snow2;
+
+function preload() {
+bgImg=loadImage("snow2.jpg");
+
+}
+
 function setup() {
-  createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+engine=Engine.create();
+world=engine.world;
+  
+createCanvas(1200,600);
+
+//After every 50 frames create a snow object
+  
+  
+  
+  
 }
 
 function draw() {
-  background(255,255,255);  
+  background(bgImg);  
+  Engine.update(engine);
+  
+  // display snow drops on the screen
+  
+  
+  
+  
   drawSprites();
 }
